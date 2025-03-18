3bf41818a7520b64351ebac80273622eac67c1f1
João Paulo <jpsilva.sousa97@outlook.com>
2025-03-18 16:27:06 -0300

    Making adjustments to translation and improvements.

--- js/scripts.js
+++ js/scripts.js
@@ -12,57 +12,93 @@ const language = {
         experience: "",
         contact: "Informações de Contato",
         about: "Sobre Minha Pessoa",
+        job: ` 
+<h3>Responsabilidades</h3>
+
+Desenvolvi, mantive e implantei aplicações de software escaláveis para web e desktop usando .NET/C#, manipulação de dados com SQL Server, criando e atualizando procedimentos existentes e MongoDb, design de Front-End com HTML, CSS e JS com JQuery incluído. Gerenciei versionamento de código e colaboração usando Azure DevOps, incluindo criação de pull requests, merge requests, revisão de código e criação de documentação para demandas.
+
+Configurei e implantei pipelines de CI/CD para ambientes de staging para testes e validação.
+
+Participei da metodologia ágil Scrum, incluindo reuniões diárias no Microsoft Teams, planejamento semanal e retrospectivas.
+
+Monitorei o desempenho da aplicação e resolvi problemas utilizando ferramentas da Amazon Web Services (AWS), como Kubernetes e ElasticSearch.
+
+Realizei testes unitários e testes de integração para garantir a integridade e confiabilidade da aplicação antes da implantação em produção.
+
+<br>
+<br>
+<h4>Principais Conquistas</h4>
+
+Entreguei com sucesso soluções de software escaláveis e sustentáveis, melhorando o desempenho da aplicação e a experiência do usuário.
+Aprimorei a colaboração da equipe e a qualidade do código através do uso eficaz do Azure DevOps e da adesão às práticas de código limpo e princípios SOLID.
+
+<h4>Tecnologias Aplicadas</h4>
+
+<ul>
+  <li>Git (Azure DevOps)</li>
+  <li>Desenvolvimento Front-End (HTML, CSS, Javascript (JQuery incluído), Bootstrap, CSS)</li>
+  <li>Back-End (C#, .Net (de 4.0 até 8.0))</li>
+  <li>Banco de Dados (MongoDB e SQL Server)</li>
+  <li>Ferramentas de Monitoramento (Kubernetes e ElasticSearch)</li>
+</ul>
+
+<h5>Arquiteturas de Projeto</h5>
+<ul>
+  <li>Microservices</li>
+  <li>MVC</li>
+  <li>Api Rest</li>
+</ul>
+   
+`,
         job2: `
- 
-        Comecei essa posição como trainee e fui promovido para junior em 7 meses. Na GR Negócios eu desenvolvia, efetuava manutenção de aplicações web e backend para melhorar sua 
-        funcionalidade, preformance e garantir que as necessidades dos clientes e da empresa seriam supridas com tais alterações.
-    utilizava uma vasta quantidade de tecnologias e frameworks, incluindo: 
-                    
-                   
-                    <br>
-                    - Back-End: Windows Forms, ASP.NET Framework, C#, MVC, .NET Core, REST APIs
-                    
-                    - Front-End: JavaScript (jQuery), AngularJS, HTML, CSS, Bootstrap
-                    
-                    - Databases: SQL Server, MySQL
-                    
-                    ORM: Entity Framework, Dapper
-                    
-                    Tools: GitLab, Jira, Confluence
-                    
-                    <h4> Projetos que trabalhei</h4>
-                    <br>
-                    <ul>
-                    <li><b>GR Cobrança (Charging System WEB)</b>:
-                      Developed a web application to assist employees in managing debt collection for clients, including negotiating payment plans and tracking agreements.</li>
-                      Password Calling System (Developed specifically for brazillian navy)</li>
-                    <li><b>GR Suíte (WEB ERP)</b></li>
-                    <li><b>Credit Analysis System (SICACRED) added new features, corrected bugs and improved a system for analyzing client credit, enabling better decision-making for our clients.</li>
-                    <li> Developed a .NET 6 ChatBot integrated with WhatsApp to automate debt collection processes</b>.
-                          The ChatBot facilitated secure payment agreements, with all deals and values automatically synced to the GR Cobrança application to facilitate the companys employers management</li>
-                    <li>Maintance and improvement of password calling system built specifically for the brazillian navy</li>
-                    </ul>
-                    
-                   <h4>Key Achievements</h4>
-                    
-                    Successfully delivered multiple high-impact projects, improving operational efficiency and client satisfaction.
-                    
-                    Demonstrated rapid growth from Trainee to Junior Developer, showcasing adaptability and technical proficiency.
-                    
-                    Automated debt collection processes through the development of a WhatsApp ChatBot, reducing manual workload and improving response times.
-                    
-                    
-                    Technologies Used
-                    
-                    Back-End: .NET, C#, ASP.NET MVC, .NET Core, REST APIs
-                    
-                    Front-End: JavaScript (jQuery), AngularJS, HTML, CSS and Bootstrap
-                    
-                    Databases: SQL Server, MySQL
-                    
-                    ORM: Entity Framework, Dapper
-                    
-                    Tools: Git, Jira, Confluence`
+ Comecei nesta empresa como Suporte de TI e sendo promovido até receber oportunidade de migrar para o setor de desenvolvimento onde após dedicação e rápido aprendizado fui promovido  para Desenvolvedor Júnior.
+<br>
+<br>
+<h4>Responsabilidades</h4>
+
+<br>
+Desenvolvi e mantive aplicações web para melhorar a funcionalidade, aprimorar o desempenho e atender às necessidades em constante evolução da empresa e de seus clientes.
+<br>
+<br>
+<h4>Projetos</h4>
+<br>
+<ul>
+  <li><b>GR Cobrança (Sistema de Cobrança WEB) (.net mvc/angularJS): </b>
+    Desenvolvi uma aplicação web para ajudar os funcionários a gerenciar a cobrança de dívidas dos clientes, incluindo a negociação de planos de pagamento e o acompanhamento dos acordos.</li>
+  <li><b>Sistema de Chamadas de Senhas (Desenvolvido especificamente para a Marinha Brasileira): </b> Sistema desenvolvido para gerenciar e facilitar o atendimento ao público em unidades da marinha.</li>
+  <br>
+  <br>
+  <li><b>GR Suíte (ERP WEB) Que permitia registrar clientes, acompanhar transações financeiras, entre outras coisas. (.Net MVC/HTML, CSS, JavaScript): </b></li>
+  <br>
+  <li><b>Sistema de Análise de Crédito (SICACRED): </b>
+    Adicionei novos recursos, corrigi erros e melhorei um sistema de análise de crédito dos clientes, permitindo uma melhor tomada de decisão para nossos clientes.</li>
+  <br>
+  <li><b>Whatsapp ChatBot: </b> Desenvolvi um ChatBot em .NET 6 integrado com o WhatsApp para automatizar os processos de cobrança de dívidas.
+    O ChatBot facilitou acordos de pagamento seguros, com todas as transações e valores sincronizados automaticamente com o aplicativo GR Cobrança, facilitando o gerenciamento dos funcionários da empresa.</li>
+  <br>
+  <li><b>Sistema de Chamadas de Senhas da Marinha: </b> Manutenção e melhoria do sistema de chamadas de senhas desenvolvido especificamente para a Marinha Brasileira.</li>
+</ul>
+
+<h4>Principais Conquistas</h4>
+
+Entreguei com sucesso múltiplos projetos de grande impacto, melhorando a eficiência operacional e a satisfação dos clientes.
+
+Demonstrei um rápido crescimento de Estagiário para Desenvolvedor Júnior, destacando minha adaptabilidade e proficiência técnica.
+
+Automatizei processos de cobrança de dívidas através do desenvolvimento de um ChatBot para WhatsApp, reduzindo a carga de trabalho manual e melhorando os tempos de resposta.
+
+<br>
+<br>
+<h4>Tecnologias Utilizadas</h4>
+
+<ul>
+  <li><b>Back-End: </b>.NET, C#, ASP.NET MVC, .NET Core, APIs REST</li>
+  <li><b>Front-End: </b> HTML, CSS, Javascript (JQuery incluído), AngularJS</li>
+  <li><b>Banco de Dados: </b> Principalmente SQL Server, mas também MySQL ocasionalmente.</li>
+  <li><b>ORMs: </b> Entity Framework, Dapper</li>
+  <li><b>Ferramentas: </b> GitLab, Jira, Confluence</li>
+</ul>
+`,
     },
     en: {
         role: "Fullstack Software Developer .Net/C#",
@@ -72,41 +108,83 @@ const language = {
  participate in meetings, always seeking to detect, correct
  and improve functionalities to attend to the necessities of
  the company and its clients.`,
-        job2: ``,
+        job: `      <h3>Responsabilities</h2>
+
+                Developed, maintained, and deployed web and desktop scalable software applications using .NET/C#, data
+                manipulation with SQL Server by creating and updating existing procedures and MongoDb , Front-End design
+                with
+                HTML, CSS and JS with JQuery included, Managed code versioning and collaboration using Azure DevOps,
+                including creating pull requests, merge requests, code reviewing and creation of documentation for
+                demands.
+
+                Configured and deployed CI/CD pipelines to staging environments for testing and validation purposes.
+
+                Participated in Scrum agile methodology, including daily meetings on microsoft-teams, weekly planning,
+                and retrospectives.
+
+                Monitored application performance and resolved issues using Amazon Web Services (AWS) tools such as
+                Kubernetes and ElasticSearch.
+
+                Conducted unit testing and integration testing to ensure application integrity and reliability before
+                production deployment.
+
+
+                <br>
+                <br>
+                <h4>Key Achievements</h4>
+
+                Successfully delivered scalable and maintainable software solutions, improving application performance
+                and user experience.
+                Enhanced team collaboration and code quality through effective use of Azure DevOps and adherence to
+                clean code practices and solid principles.
+                <h4>Technologies applied</h4>
+
+                <ul>
+                  <li>Git (Azure Devops)</li>
+                  <li>Front-End Development (HTML, CSS, Javascript (Jquery included), Bootstrap, CSS.</li>
+                  <li>Back-End (C#, .Net (from 4.0 up to 8.0)</li>
+                  <li>Database (MongoDB and SQL Server)</li>
+                  <li>Monitoring Tools (Kubernetes and ElasticSearch)</li>
+                </ul>
+
+                <h5>Project Architectures</h5>
+                <ul>
+                  <li>Microservices</li>
+                  <li>MVC</li>
+                  <li>Api Rest</li>
+                </ul>`,
+
         contact: "Contact Information",
         about: "About Me",
         job2: `              
-                    
-       
+              
+        Started in this company as I.T Support and worked my way up to Junior developer by first becoming a trainee and then getting promoted after 7 months for showing dedication, 
+        fast learning and problem solving capabilities. 
+          <br>
+        <br>
         <h4>Responsabilities</h4>
+
          <br>
-       Started this position as a traineee and got promoted to Junior within 7 months. Developed and maintained web applications to enhance functionality, improve performance, and meet the evolving needs of the company and its clients.
-                    
-                    Utilized a wide range of technologies and frameworks, including:
+       Developed and maintained web applications to enhance functionality, improve performance, and meet the evolving needs of the company and its clients.
                     <br>
-                    - Back-End: Windows Forms, ASP.NET Framework, C#, MVC, .NET Core, REST APIs
-                    
-                    - Front-End: JavaScript (JQuery), AngularJS, HTML, CSS, Bootstrap
-                    
-                    - Databases: SQL Server, MySQL
-                    
-                    ORM: Entity Framework, Dapper
-                    
-                    Tools: GitLab, Jira, Confluence
-                    <br>
-                    <br>
+                  <br>
                     <h4> Projects</h4>
                     <br>
                     <ul>
                     <li><b>GR Cobrança (Charging System WEB) (.net mvc/angularJS): </b>
                       Developed a web application to assist employees in managing debt collection for clients, including negotiating payment plans and tracking agreements.</li>
                       Password Calling System (Developed specifically for brazillian navy)</li>
-                    <li><b>GR Suíte (WEB ERP) (.Net MVC/HTML, CSS, JavaScript): </b></li>
+                    <br>
+                      <br>
+                      <li><b>GR Suíte (WEB ERP) Which you could register clients, keep track finantial transactions among other things.  (.Net MVC/HTML, CSS, JavaScript): </b></li>
+                    <br>
                     <li><b>Credit Analysis System (SICACRED): </b>
                      added new features, corrected bugs and improved a system for analyzing client credit, enabling better decision-making for our clients.</li>
-                    <li><b>Whatsapp ChatBot: </b> Developed a .NET 6 ChatBot integrated with WhatsApp to automate debt collection processes.
+                    <br>
+                     <li><b>Whatsapp ChatBot: </b> Developed a .NET 6 ChatBot integrated with WhatsApp to automate debt collection processes.
                           The ChatBot facilitated secure payment agreements, with all deals and values automatically synced to the GR Cobrança application to facilitate the companys employees management</li>
-                    <li><b> Navy Password Calling System: </b>Maintance and improvement of password calling system built specifically for the brazillian navy</li>
+                    <br>
+                          <li><b> Navy Password Calling System: </b>Maintance and improvement of password calling system built specifically for the brazillian navy</li>
                     </ul>
                     
                    <h4>Key Achievements</h4>
@@ -117,26 +195,29 @@ const language = {
                     
                     Automated debt collection processes through the development of a WhatsApp ChatBot, reducing manual workload and improving response times.
                     
-                    
+                    <br>
+                    <br>
                     <h4>Technologies Used</h4>
                     
-                    Back-End: .NET, C#, ASP.NET MVC, .NET Core, REST APIs
+                    <ul>
+                    <li><b>Back-End: </b>.NET, C#, ASP.NET MVC, .NET Core, REST APIs</li>
+                     <li><b>Front-End: HTML, CSS, Javascript (Jquery included), AngularJS</b></li>  
+                     <li><b>Databases: </b> Mostly SQL Server but also MySQL ocasionally.
+                     <li><b>ORMs: Entity Framework, Dapper</b>: 
+                     <Li><b>Tools: </b> GitLab, Jira, Confluence</li>
+                     </ul>
                     
-                    Front-End: JavaScript (jQuery), AngularJS, HTML, CSS and Bootstrap
                     
-                    Databases: SQL Server, MySQL
-                    
-                    ORM: Entity Framework, Dapper
-                    
-                    Tools: Git, Jira, Confluence
             `
     },
  
 };
 
-$(document).ready(function() {
+const languageSelect = document.querySelector('#language-select');
 
-    const languageSelect = document.querySelector('#language-select');
+
+
+$(document).ready(function() {
 
     languageSelect.addEventListener('change', () => {
         
@@ -148,14 +229,13 @@ $(document).ready(function() {
 
             document.getElementById('role').textContent = language.pt.role; 
             document.getElementById('experienceHeader').textContent = 'Experiência'; 
-            document.getElementById('downloadResumeBtn').textContent = 'Baixar Currículo'; 
-            document.getElementById('job2Paragraph').textContent = `I worked for two years, from 07/2022 to 09/2024, where I started as a trainee and, 
-            after 7 months, was promoted to junior. My responsibilities included developing and maintaining web applications to fix and improve their functionality to better meet the 
-            needs of the company and its clients. I also performed the following activities:`;
+            document.getElementById('downloadResumeBtn').textContent = 'Baixar Currículo';
             document.getElementById('education').textContent = language.pt.education;
             document.getElementById('contactInfo').textContent = language.pt.contact;
             document.getElementById('aboutHeader').textContent = language.pt.about;
-            document.getElementById('job2Paragraph').innerHTML = language.pt.job2;
+            document.getElementById('jobCard').innerHTML = language.pt.job;
+            document.getElementById('jobCard2').innerHTML = language.pt.job2;
+       
            
         } else {
             
@@ -167,8 +247,8 @@ $(document).ready(function() {
             document.getElementById('education').textContent = language.en.education;
             document.getElementById('contactInfo').textContent = language.en.contact;
             document.getElementById('aboutHeader').textContent = language.en.about;
-          
-            document.getElementById('job2Paragraph').innerHTML = language.en.job2; 
+            document.getElementById('jobCard').innerHTML = language.en.job;
+            document.getElementById('jobCard2').innerHTML = language.en.job2; 
         }
     });
 });
